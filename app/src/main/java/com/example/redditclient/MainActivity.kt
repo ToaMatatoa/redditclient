ffc93e08476f27c56db747191250d58b6d427332
Matatoa <rikonikotako@gmail.com>
2021-07-07 14:35:07 +0300

    Added fragment for reddit posts changed main activity for adding view binding, kodein di and include fragmnet changed fragment for di and view binding changed build.gradle for view binding

--- app/src/main/java/com/example/redditclient/MainActivity.kt
+++ app/src/main/java/com/example/redditclient/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.redditclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.redditclient.databinding.ActivityMainBinding
+import com.example.redditclient.ui.Fragment
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.KodeinTrigger
+import org.kodein.di.android.closestKodein
+import org.kodein.di.android.retainedKodein
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), KodeinAware {
+
+    private val parentKodein: Kodein by closestKodein()
+    override val kodein: Kodein by retainedKodein {
+        extend(parentKodein)
+    }
+    override val kodeinTrigger = KodeinTrigger()
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        kodeinTrigger.trigger()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        if (savedInstanceState == null) {
+            val fragment = Fragment()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
     }
 }
--- app/src/main/java/com/example/redditclient/ui/Fragment.kt
+++ app/src/main/java/com/example/redditclient/ui/Fragment.kt
@@ -1,4 +1,43 @@
 package com.example.redditclient.ui
 
-class Fragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.redditclient.R
+import com.example.redditclient.databinding.FragmentRedditBinding
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.KodeinTrigger
+import org.kodein.di.android.x.closestKodein
+import org.kodein.di.generic.kcontext
+
+class Fragment : Fragment(R.layout.fragment_reddit), KodeinAware {
+
+    override val kodeinContext = kcontext<Fragment>(this)
+    private val parentKodein: Kodein by closestKodein()
+    override val kodein: Kodein = Kodein.lazy {
+        extend(parentKodein)
+    }
+    override val kodeinTrigger = KodeinTrigger()
+
+    private var _binding: FragmentRedditBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        kodeinTrigger.trigger()
+
+        _binding = FragmentRedditBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
