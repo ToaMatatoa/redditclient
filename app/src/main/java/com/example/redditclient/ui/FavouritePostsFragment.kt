cac023807a5aec455026351915292fa86bd0bff9
Matatoa <rikonikotako@gmail.com>
2021-07-07 15:18:09 +0300

    add fragment for favourite posts (xml and class), added Navigation

---
+++ app/src/main/java/com/example/redditclient/ui/FavouritePostsFragment.kt
@@ -0,0 +1,43 @@
+package com.example.redditclient.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.redditclient.R
+import com.example.redditclient.databinding.FragmentFavouriteBinding
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.KodeinTrigger
+import org.kodein.di.android.x.closestKodein
+import org.kodein.di.generic.kcontext
+
+class FavouritePostsFragment : Fragment(R.layout.fragment_favourite), KodeinAware {
+
+    override val kodeinContext = kcontext<Fragment>(this)
+    private val parentKodein: Kodein by closestKodein()
+    override val kodein: Kodein = Kodein.lazy {
+        extend(parentKodein)
+    }
+    override val kodeinTrigger = KodeinTrigger()
+
+    private var _binding: FragmentFavouriteBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        kodeinTrigger.trigger()
+
+        _binding = FragmentFavouriteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
