463c3ed7c3603cd84b91a06b6fc5547337157aa1
Matatoa <rikonikotako@gmail.com>
2021-07-13 11:08:05 +0300

    minor fixes

--- app/src/main/java/com/example/redditclient/ui/FavoritePostsAdapter.kt
+++ app/src/main/java/com/example/redditclient/ui/FavoritePostsAdapter.kt
@@ -8,15 +8,22 @@ import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
 import com.example.redditclient.R
 import com.example.redditclient.data.local.model.FavoritePost
-import com.example.redditclient.databinding.RvAllItemBinding
+import com.example.redditclient.databinding.RvFavoriteItemBinding
 
-class FavoritePostsAdapter(private var listener: OnItemClickListener) :
+class FavoritePostsAdapter(
+    private var listener: OnItemClickListener,
+    private var favoriteListener: OnFavoriteClickListener
+) :
     RecyclerView.Adapter<FavoritePostsAdapter.ViewHolder>() {
 
     private val items = mutableListOf<FavoritePost>()
 
     interface OnItemClickListener {
         fun onItemClick(position: Int)
+    }
+
+    interface OnFavoriteClickListener {
+        fun onFavoriteClick(post: FavoritePost)
     }
 
     fun addFavoritePosts(favoritePosts: List<FavoritePost>) {
@@ -27,21 +34,24 @@ class FavoritePostsAdapter(private var listener: OnItemClickListener) :
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val itemBinding =
-            RvAllItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            RvFavoriteItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(itemBinding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val postPosition: FavoritePost = items[position]
-        holder.bind(postPosition, listener)
+        holder.bind(postPosition, listener, favoriteListener)
     }
 
     override fun getItemCount(): Int = items.size
 
-    class ViewHolder(private var itemBinding: RvAllItemBinding) :
+    class ViewHolder(private var itemBinding: RvFavoriteItemBinding) :
         RecyclerView.ViewHolder(itemBinding.root) {
 
-        fun bind(item: FavoritePost, listener: OnItemClickListener?) =
+        fun bind(
+            item: FavoritePost, listener: OnItemClickListener?,
+            favoriteListener: OnFavoriteClickListener?
+        ) =
             with(itemView) {
                 Glide.with(this)
                     .load(item.thumbnail)
@@ -57,6 +67,16 @@ class FavoritePostsAdapter(private var listener: OnItemClickListener) :
 
                 if (listener != null) {
                     itemBinding.root.setOnClickListener { listener.onItemClick(layoutPosition) }
+                }
+
+                itemBinding.ivFavorite.isChecked = false
+
+                if (favoriteListener != null) {
+                    itemBinding.ivFavorite.setOnCheckedChangeListener { _, _ ->
+                        favoriteListener.onFavoriteClick(
+                            item
+                        )
+                    }
                 }
             }
     }
--- app/src/main/java/com/example/redditclient/ui/FavouritePostsFragment.kt
+++ app/src/main/java/com/example/redditclient/ui/FavouritePostsFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.redditclient.Constants.ARG_POSITION
 import com.example.redditclient.R
+import com.example.redditclient.data.local.model.FavoritePost
 import com.example.redditclient.databinding.FragmentFavouriteBinding
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
@@ -19,7 +20,7 @@ import org.kodein.di.generic.instance
 import org.kodein.di.generic.kcontext
 
 class FavouritePostsFragment : Fragment(R.layout.fragment_favourite),
-    FavoritePostsAdapter.OnItemClickListener, KodeinAware {
+    FavoritePostsAdapter.OnItemClickListener, FavoritePostsAdapter.OnFavoriteClickListener, KodeinAware {
 
     override val kodeinContext = kcontext<Fragment>(this)
     private val parentKodein: Kodein by closestKodein()
@@ -38,6 +39,11 @@ class FavouritePostsFragment : Fragment(R.layout.fragment_favourite),
         context?.let { viewModel.openEntryInChromeTabFromFavorite(position, it) }
     }
 
+    override fun onFavoriteClick(post: FavoritePost) {
+        viewModel.deletePost(post.id)
+        viewModel.loadFavoritePosts()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -52,7 +58,7 @@ class FavouritePostsFragment : Fragment(R.layout.fragment_favourite),
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        favoritePostsAdapter = FavoritePostsAdapter(this)
+        favoritePostsAdapter = FavoritePostsAdapter(this, this)
 
         viewModel.liveDataLocalProvider.observe(viewLifecycleOwner, { localPosts ->
             favoritePostsAdapter?.addFavoritePosts(localPosts)
@@ -69,6 +75,11 @@ class FavouritePostsFragment : Fragment(R.layout.fragment_favourite),
         binding.srlFavoritePosts.setOnRefreshListener {
             refreshAllPosts()
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.loadFavoritePosts()
     }
 
     override fun onDestroyView() {
